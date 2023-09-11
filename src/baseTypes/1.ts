a921c83d69ff55ded320ad9ac71b86c3d47730bf
Marat Baikin <3371335@gmail.com>
2023-09-11 16:35:02 +0300

    Update 1.ts

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,10 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+let age: number
+let name : string
+let toggle: boolean
+let empty: null;
+let notInitialize: undefined
+let callback = (a: number): number => { return 100 + a };
 
 export {};
