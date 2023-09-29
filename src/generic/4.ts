9b71d5c1c54e92bcb3a735eb2fc60772aa4e3629
Marat Baikin <3371335@gmail.com>
2023-09-29 11:49:27 +0300

    Complited

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,17 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+interface Icomponents {
+  title: string
+}
+
+class Component <T extends Icomponents> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component <Icomponents> {
   pageInfo () {
     console.log(this.props.title);
   }
